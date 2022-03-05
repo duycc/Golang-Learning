666a01c8fea5512c77f1de66c8b4428d4d5b50a4
duycc <gz_duyong@163.com>
2022-03-06 02:07:09 +0800

    2022-03-06

--- eight-hour/14-IM-System/server.go
+++ eight-hour/14-IM-System/server.go
@@ -3,24 +3,59 @@ package main
 import (
 	"fmt"
 	"net"
+	"sync"
 )
 
 type Server struct {
 	Ip   string
 	Port int
+
+	OnlineUsers map[string]*User // 所有在线用户
+	mapLock     sync.RWMutex     // 读写锁
+	Message     chan string      // 消息广播
 }
 
 func NewServer(ip string, port int) *Server {
 	server := &Server{
-		Ip:   ip,
-		Port: port,
+		Ip:          ip,
+		Port:        port,
+		OnlineUsers: make(map[string]*User),
+		Message:     make(chan string),
 	}
 	return server
 }
 
+func (s *Server) ListenMessage() {
+	for {
+		msg := <-s.Message
+
+		s.mapLock.Lock()
+		for _, cli := range s.OnlineUsers {
+			cli.C <- msg
+		}
+		s.mapLock.Unlock()
+	}
+}
+
+// 广播消息
+func (s *Server) BroadCast(user *User, msg string) {
+	sendMsg := "[" + user.Addr + "]" + user.Name + ": " + msg
+	s.Message <- sendMsg
+}
+
 func (s *Server) Handler(conn net.Conn) {
 	// 业务逻辑
-	fmt.Println("connect succ...")
+	// fmt.Println("connect succ...")
+
+	user := NewUser(conn)
+
+	s.mapLock.Lock()
+	s.OnlineUsers[user.Name] = user
+	s.mapLock.Unlock()
+
+	s.BroadCast(user, "already online.")
+
+	select {} // 阻塞当前handler
 }
 
 func (s *Server) Start() {
@@ -29,6 +64,8 @@ func (s *Server) Start() {
 		fmt.Println("net.Listen err: ", err)
 	}
 	defer listener.Close()
+	go s.ListenMessage()
+	fmt.Printf("server start, listening at %s:%d\n", s.Ip, s.Port)
 
 	for {
 		conn, err := listener.Accept()
---
+++ eight-hour/14-IM-System/user.go
@@ -0,0 +1,30 @@
+package main
+
+import "net"
+
+type User struct {
+	Name string
+	Addr string
+	C    chan string
+	conn net.Conn
+}
+
+func NewUser(conn net.Conn) *User {
+	userAddr := conn.RemoteAddr().String()
+	user := &User{
+		Name: userAddr,
+		Addr: userAddr,
+		C:    make(chan string),
+		conn: conn,
+	}
+	go user.ListenMessage()
+	return user
+}
+
+// 监听User chan，有消息便发送给对端
+func (u *User) ListenMessage() {
+	for {
+		msg := <-u.C
+		u.conn.Write([]byte(msg + "\n"))
+	}
+}
