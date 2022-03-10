7d412c9c2ed70c8a5de9d0db6939abc5accfaa54
duycc <gz_duyong@163.com>
2022-03-10 15:13:00 +0800

    2022-03-10

--- gorm/main.go
+++ gorm/main.go
@@ -18,12 +18,12 @@ type Food struct {
 	CreateTime time.Time
 }
 
-func (v Food) TableName() string {
+func (f Food) TableName() string {
 	return "foods"
 }
 
 func main() {
-	fmt.Println("hello gorm...")
+	// fmt.Println("hello gorm...")
 	db, err := getDB()
 	if err != nil {
 		panic("conn mysql failed, error: " + err.Error())
@@ -31,13 +31,20 @@ func main() {
 
 	// insertData(db)
 
-	queryData(db)
+	// queryData(db)
+
+	updateRecords(db)
 
 	defer db.Close()
 }
 
+func updateRecords(db *gorm.DB) {
+	fmt.Println("updateRecords test...")
+}
+
 func queryData(db *gorm.DB) {
 	// food := Food{}
+
 	// if err := db.Take(&food).Error; err != nil {
 	// 	fmt.Println("query failed.")
 	// 	return
@@ -59,14 +66,82 @@ func queryData(db *gorm.DB) {
 	// 	return
 	// }
 
-	var titles []string
-	if err := db.Model(&Food{}).Pluck("title", &titles).Error; err != nil {
-		fmt.Println("query failed.")
-		return
-	}
+	//
 
-	fmt.Println("query ok.")
-	fmt.Println(titles)
+	// err := db.Where("id = ?", 2).Take(&food).Error
+	// if errors.Is(err, gorm.ErrRecordNotFound) {
+	// 	fmt.Println("record not found.")
+	// 	return
+	// } else if err != nil {
+	// 	fmt.Println("query failed.")
+	// 	return
+	// }
+	// if err := db.Where("id = ?", 2).Take(&food).Error; err != nil {
+	// 	fmt.Printf("query failed, err: %+v\n", err)
+	// 	return
+	// }
+
+	// if err := db.Where("id in (?)", []int{1, 2}).Take(&food).Error; err != nil {
+	// 	fmt.Printf("query failed, err: %+v\n", err)
+	// 	return
+	// }
+
+	// foods := make([]Food, 10)
+
+	// if err := db.Where("title like ?", "%an%").Find(&foods).Error; err != nil {
+	// 	fmt.Printf("query failed, err: %+v\n", err)
+	// 	return
+	// }
+	// fmt.Println("query ok.")
+	// fmt.Println(foods)
+
+	// food := Food{}
+	// // if err := db.Select("id, title").Where("id = ?", 1).Take(&food).Error; err != nil {
+	// if err := db.Select([]string{"id", "title"}).Where("id = ?", 1).Take(&food).Error; err != nil {
+	// 	fmt.Printf("query failed, err: %+v\n", err)
+	// 	return
+	// }
+	// fmt.Println(food)
+
+	// total := []int{}
+	// if err := db.Model(&Food{}).Select("count(*) as total").Pluck("total", &total).Error; err != nil {
+	// 	fmt.Printf("query failed, err: %+v\n", err)
+	// 	return
+	// }
+	// fmt.Println(total)
+
+	// foods := []Food{}
+	// if err := db.Where("id in (?)", []int{1, 2}).Order("create_time desc").Find(&foods).Error; err != nil {
+	// 	fmt.Printf("query failed, err: %+v\n", err)
+	// 	return
+	// }
+
+	// if err := db.Order("create_time desc").Limit(1).Offset(0).Find(&foods).Error; err != nil {
+	// 	fmt.Printf("query failed, err: %+v\n", err)
+	// 	return
+	// }
+	// fmt.Println(foods)
+
+	// var total int64
+	// db.Model(&Food{}).Count(&total)
+	// fmt.Println("count: ", total)
+
+	results := []Result{}
+	// if err := db.Model(&Food{}).Select("type, count(*) as total").Group("type").Having("total > 0").Scan(&results).Error; err != nil {
+	// 	fmt.Printf("query failed, err: %+v\n", err)
+	// 	return
+	// }
+	// fmt.Println(results)
+
+	// 直接执行sql语句
+	sql := "select type, count(*) as total from `foods` where create_time > ? group by type having (total > 0)"
+	db.Raw(sql, "2022-02-21 01:04:07").Scan(&results)
+	fmt.Println(results)
+}
+
+type Result struct {
+	Type  int
+	Total int
 }
 
 func insertData(db *gorm.DB) {
