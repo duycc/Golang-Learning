4aa6262c1509a758390955ea19737a45f9104a7e
duyong.9511 <duyong.9511@bytedance.com>
2022-04-26 10:10:47 +0800

    grpc

--- Go微服务实战/ch13/13.3/grpc/client/client.go
+++ Go微服务实战/ch13/13.3/grpc/client/client.go
@@ -1,34 +1,39 @@
 package main
+
 import (
 	"context"
 	"fmt"
+
+	"Golang-Learning/Go微服务实战/ch13/13.3/grpc/protocol/protocol"
+
 	"google.golang.org/grpc"
-	pt "../protocol"
 )
+
 const (
-	post  = "127.0.0.1:18887"
+	post = "127.0.0.1:18887"
 )
+
 func main() {
 	// 客户端连接服务器
-	conn,err:=grpc.Dial(post,grpc.WithInsecure())
-	if err!=nil {
-		fmt.Println("连接服务器失败",err)
+	conn, err := grpc.Dial(post, grpc.WithInsecure())
+	if err != nil {
+		fmt.Println("连接服务器失败", err)
 	}
 
 	defer conn.Close()
 
 	//获得grpc句柄
-	c:=pt.NewHelloServerClient(conn)
+	c := protocol.NewHelloServerClient(conn)
 
 	//远程调用 SayHello接口
-	r1, err := c.SayHello(context.Background(), &pt.HelloRequest{Name: "Scott"})
+	r1, err := c.SayHello(context.Background(), &protocol.HelloRequest{Name: "Scott"})
 	if err != nil {
 		fmt.Println("cloud not get Hello server ..", err)
 		return
 	}
 	fmt.Println("HelloServer resp: ", r1.Message)
 	//远程调用 GetHelloMsg接口
-	r2, err := c.GetHelloMsg(context.Background(), &pt.HelloRequest{Name: "Scott"})
+	r2, err := c.GetHelloMsg(context.Background(), &protocol.HelloRequest{Name: "Scott"})
 	if err != nil {
 		fmt.Println("cloud not get hello msg ..", err)
 		return
--- Go微服务实战/ch13/13.3/grpc/server/server.go
+++ Go微服务实战/ch13/13.3/grpc/server/server.go
@@ -1,35 +1,43 @@
 package main
+
 import (
 	"context"
 	"fmt"
-	"google.golang.org/grpc"
-	pt "../protocol"
 	"net"
+
+	pt "Golang-Learning/Go微服务实战/ch13/13.3/grpc/protocol/protocol"
+
+	"google.golang.org/grpc"
 )
+
 const (
-	post  = "127.0.0.1:18887"
+	post = "127.0.0.1:18887"
 )
+
 //对象要和proto内定义的服务一样
 type server struct{}
+
 //实现RPC SayHello 接口
-func(s *server)SayHello(ctx context.Context,in *pt.HelloRequest)(*pt.HelloReplay , error){
-	return  &pt.HelloReplay{Message:"hello"+in.GetName()},nil
+func (s *server) SayHello(ctx context.Context, in *pt.HelloRequest) (*pt.HelloReplay, error) {
+	return &pt.HelloReplay{Message: "hello" + in.GetName()}, nil
 }
+
 //实现RPC GetHelloMsg 接口
 func (s *server) GetHelloMsg(ctx context.Context, in *pt.HelloRequest) (*pt.HelloMessage, error) {
 	return &pt.HelloMessage{Msg: "this is from server!"}, nil
 }
 func main() { //监听网络
-	ln ,err :=net.Listen("tcp",post)
-	if err!=nil {
-		fmt.Println("网络异常",err) }
+	ln, err := net.Listen("tcp", post)
+	if err != nil {
+		fmt.Println("网络异常", err)
+	}
 	// 创建一个grpc的句柄
-	srv:= grpc.NewServer()
+	srv := grpc.NewServer()
 	//将server结构体注册到 grpc服务中
-	pt.RegisterHelloServerServer(srv,&server{})
+	pt.RegisterHelloServerServer(srv, &server{})
 	//监听grpc服务
-	err= srv.Serve(ln)
-	if err!=nil {
-		fmt.Println("网络启动异常",err)
+	err = srv.Serve(ln)
+	if err != nil {
+		fmt.Println("网络启动异常", err)
 	}
 }
